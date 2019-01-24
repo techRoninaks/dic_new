28f52221daf1087fd509c47dcc6111f6101a64b4
Srinath <srinathkamath@roninaks.com>
2019-01-24 11:29:05 +0530

    changes made to js and css and added scripts.js to html files

--- admin/assets/js/scripts.js
+++ admin/assets/js/scripts.js
@@ -39,3 +39,51 @@ function delete_news(id){
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send(params);
 }
+
+function delete_news(id){
+  var xhr =  new XMLHttpRequest();
+  var params = 'id='+id;
+    this.responseType = 'text';
+    xhr.onreadystatechange  =  function() {
+        if (this.readyState == 4 && this.status == 200) {//if result successful
+                document.getElementById("fetch").innerHTML = "<table class='table table-hover table-striped'><thead><th>ID</th><th>DATE</th><th>HEADLINE</th><th>SUBHEADLINE</th><th>CONTENT</th><th>ACTIONS</th></thead><tbody>" + xhr.responseText + "</tbody></table>";
+        }
+    };
+    xhr.open("POST", "edit-news.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(params);
+}
+
+function fetch_news() {
+    var xhr =  new XMLHttpRequest();
+    this.responseType = 'text';
+    xhr.onreadystatechange  =  function() {
+        if (this.readyState == 4 && this.status == 200) {//if result successful
+                document.getElementById("fetch").innerHTML = "<table class='table table-hover table-striped'>" + xhr.responseText + "</table>";
+        }
+    };
+    xhr.open("GET", "fetch-news.php", true);
+    xhr.setRequestHeader("Content-type", "text/plain");
+    xhr.send();
+}
+function update_news(){
+
+    var data1 = document.getElementById("Headline").value;
+    var data2 = document.getElementById("Subheadline").value;
+    var data3 = document.getElementById("News").value;
+    var xhr =  new XMLHttpRequest();
+    this.responseType = 'valom nadakuo?';
+    xhr.onreadystatechange  =  function() {
+        if (this.readyState == 4 && this.status == 200) {//if result successful
+                //console.log(xhr.responseText);
+                //document.getElementById("upnews").value;
+        }
+    };
+    if(confirm("Press 'OK' to confirm")){
+        xhr.open("POST", "update-news.php", true);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        var params = 'Headline='+data1+'&Subheadline='+data2+'&News='+data3;
+        xhr.send(params);
+        location.reload();
+    }   
+}
