f6208459ba12f37b0f69d290546de34d441562c7
ashishkamal17 <ashishak@roninaks.com>
2019-01-24 11:31:29 +0530

    additional pages for DIC Commit 2

--- dynamicloading.js
+++ dynamicloading.js
@@ -22,6 +22,9 @@ function showData(str, caller1) {
         case "industry":
             xhr.open("GET", "getindustry.php", true);//other php files here
             break;
+        case "vya":
+        xhr.open("GET", "getmagazine.php", true);
+            break;
         default:
             ;
     }
@@ -42,6 +45,10 @@ function showData(str, caller1) {
         case "industry":
             document.getElementById(newstr[0]).innerHTML = ourData[0];
             break;
+        case "vya":
+            console.log(newstr[0]+" hell "+ ourData[0]);
+            document.getElementById(newstr[0]).innerHTML = ourData[0];
+            break;
 
             default:
             ;
@@ -52,3 +59,43 @@ function showData(str, caller1) {
 
     
   };
+
+  function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("w3-include-html");
+      if (file) {
+        /* Make an HTTP request using the attribute value as the file name: */
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {
+                elmnt.innerHTML = this.responseText;
+            }
+            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+            /* Remove the attribute, and call this function once more: */
+            elmnt.removeAttribute("w3-include-html");
+            includeHTML();
+          }
+        } 
+        xhttp.open("GET", file, true);
+        xhttp.send();
+
+        /* Exit the function: */
+        return;
+      }
+    }
+    // window.onload = newFunction();
+    
+    
+
+    //   function newFunction() {
+
+    //   }
+    // console.log("Hell onload");
+    // document.getElementById("#myHeader").style.background = "#fff";
+} 
